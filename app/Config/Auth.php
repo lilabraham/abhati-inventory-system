c98b0750cee04aae7508a5e37049603c20979eb9
Iqra Manaqibal Atqiya <iqroace1@gmail.com>
2026-06-19 16:17:21 +0700

    config: restrict login to username-only via Shield validFields

--- app/Config/Auth.php
+++ app/Config/Auth.php
@@ -284,7 +284,6 @@ class Auth extends ShieldAuth
      * Fields that are available to be used as credentials for login.
      */
     public array $validFields = [
-        'email',
         'username',
     ];
 
